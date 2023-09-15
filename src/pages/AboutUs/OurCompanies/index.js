3f0f95403cef8236c8f19bb06134ad462527adc0
ThanhNguyen <nguyentrungthanh160399@gmail.com>
2023-09-15 15:22:46 +0700

    update

--- src/pages/AboutUs/OurCompanies/index.js
+++ src/pages/AboutUs/OurCompanies/index.js
@@ -1,7 +1,7 @@
 import CompaniesStyle from './ourcompanies.module.scss'
-import WhoWeAre from "../../../Image/AboutUs/Companies/WhoWeAre_EN_updated20230704.jpg"
-import fvlOffice1 from "../../../Image/AboutUs/Companies/FVL-Office.jpg"
-import fvlOffice2 from "../../../Image/AboutUs/Companies/FVL-Office2.jpg"
+import WhoWeAre from "../../../Image/AboutUs/Our_Companies/WhoWeAre_EN_updated20230704.jpg"
+import fvlOffice1 from "../../../Image/AboutUs/Our_Companies/FVL-Office.jpg"
+import fvlOffice2 from "../../../Image/AboutUs/Our_Companies/FVL-Office2.jpg"
 import {Introduction} from "../../../components/EmbeddedGoogleDriveFile/DriveFilePage"
 const OurCompanies = () => { 
     
