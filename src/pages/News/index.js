cecf2ac306e5ae9f114e2ed564bd9eb9685cc722
unknown <nguyentrungthanh160399@gmail.com>
2023-09-17 23:54:23 +0700

    update

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -1,7 +1,25 @@
+import {contentEvents} from '../../components/News'
+
+
+
 const News = () => {
+
     return ( 
         <>
-            News Page
+            {
+                contentEvents.map((contentItem, index) => {
+                    return (
+                        <div key={index} >
+                            <a href={contentItem.link} target="_blank" rel="noopener noreferrer">
+                                <img src={contentItem.urlImage} alt={contentItem.alt}/>
+                                <div>{contentItem.title}</div>
+                                <div>{contentItem.content}</div>
+                                
+                            </a>
+                        </div>
+                    )
+                })
+            }
         </>
      );
 }
