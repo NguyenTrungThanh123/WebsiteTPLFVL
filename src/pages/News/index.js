15ccc8cf9d49aebd59f00198e274012c3aba65da
unknown <nguyentrungthanh160399@gmail.com>
2023-09-25 23:10:48 +0700

    update

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -1,13 +1,26 @@
 import {contentEvents} from '../../components/News'
-
-
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
 const News = () => {
 
+    const [itemOffset, setItemOffset] = useState(0)
+    const itemsPerPage = 9;
+    const endOffset = itemOffset + itemsPerPage;
+    const currentData = contentEvents.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(contentEvents.length / itemsPerPage);
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % contentEvents.length;
+        setItemOffset(newOffset);
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth', // Tạo hiệu ứng cuộn mượt
+          });
+      };
     return ( 
         <>
             {
-                contentEvents.map((contentItem, index) => {
+                currentData.map((contentItem, index) => {
                     return (
                         <div key={index} >
                             <a href={contentItem.link} target="_blank" rel="noopener noreferrer">
@@ -20,6 +33,16 @@ const News = () => {
                     )
                 })
             }
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                previousLabel="< previous"
+                renderOnZeroPageCount={null}
+            />
+
         </>
      );
 }
