84570415343862c53b619b66f447ba0f79abff4c
ThanhNguyen <nguyentrungthanh160399@gmail.com>
2023-10-08 10:13:45 +0700

    update

--- src/components/Layout/OutsourcLayout/index.js
+++ src/components/Layout/OutsourcLayout/index.js
@@ -4,13 +4,14 @@ import home from '../../../pages/Home/home.module.scss'
 import osLayout from './OSLayout.module.scss'
 import Sidebar from "../../SidebarOutsourcing";
 import { Outlet } from "react-router-dom";
-
+import OutsourcingBanner from '../../../components/Banner/OutsourcingBanner'
 
 
 const outsourcLayout = () => {
     return (
         <>
             <div className={home.content}>
+                <OutsourcingBanner />
                 <div className='container-fluid'>
                     <div className="col-lg-12">
                         <div className="row">
